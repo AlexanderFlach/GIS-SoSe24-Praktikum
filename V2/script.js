758095b06ab4f0a557f6d5e12f7a2b87dc3ecaf8
Alexander <alexander.flach2004@web.de>
2024-05-22 01:57:02 +0200

    check Username, Password, PIN

--- V2/script.js
+++ V2/script.js
@@ -3,16 +3,51 @@ var bankCardImage = document.querySelector("#imageBankCard")
 var twitterImage = document.querySelector("#imageTwitter")
 var instagramImage = document.querySelector("#imageInstagram")
 
+var loginForm = document.getElementById("loginForm");
+var transactionForm = document.getElementById("transactionForm");
+
 
 fetch("data.json")
     .then(response => response.json())
 
     .then(json => {
-        console.log(json);
-
         // Images
-        logoImage.src = json.image1;
-        bankCardImage.src = json.image2;
-        twitterImage.src = json.image3;
-        instagramImage.src = json.image4;
+        if(logoImage != null) {logoImage.src = json.image1;}
+        if(bankCardImage != null) {bankCardImage.src = json.image2;}
+        if(twitterImage != null) {twitterImage.src = json.image3;}
+        if(instagramImage != null) {instagramImage.src = json.image4;}
+        
+        if(loginForm != null){
+            loginForm.addEventListener("submit", (e) => {
+                var username = document.getElementById("username").value;
+                var password = document.getElementById("password").value;
+            
+                if(username != json.accountData.username){
+                    e.preventDefault();
+                    document.getElementById("username").value = null;
+                    document.getElementById("username").placeholder = "Wrong Username!";
+                }
+                if(password != json.accountData.password){
+                    e.preventDefault();
+                        document.getElementById("password").value = null;
+                        document.getElementById("password").placeholder = "Wrong Password!";
+                }
+            })
+        }
+
+        if(transactionForm != null){
+            transactionForm.addEventListener("submit", (e) => {
+                e.preventDefault();
+                var pin = document.getElementById("pin").value;
+
+                if(pin == json.accountData.pin) {
+                    console.log("PIN correct!");
+                }
+                
+            })
+        }
 })
+
+
+
+
