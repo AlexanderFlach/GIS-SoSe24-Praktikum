0c385a17bcc1d591646888a8e3557b904ffc84ad
Alexander <alexander.flach2004@web.de>
2024-05-22 17:35:22 +0200

    Deposit, Withdraw, Transaction History

--- V2/script.js
+++ V2/script.js
@@ -6,6 +6,9 @@ var instagramImage = document.querySelector("#imageInstagram")
 var loginForm = document.getElementById("loginForm");
 var transactionForm = document.getElementById("transactionForm");
 
+function add(a, b){
+    return a + b;
+}
 
 fetch("data.json")
     .then(response => response.json())
@@ -17,8 +20,17 @@ fetch("data.json")
         if(twitterImage != null) {twitterImage.src = json.image3;}
         if(instagramImage != null) {instagramImage.src = json.image4;}
         
+        if(document.getElementById("currentBalance") != null) {
+            document.getElementById("currentBalance").innerHTML += json.balance;
+        }
+        if(document.getElementById("thTitle") != null) {
+            document.getElementById("thTitle").innerHTML = json.transHistory[0].title;
+        }
+        
+        //Event Listener for login
         if(loginForm != null){
-            loginForm.addEventListener("submit", (e) => {
+            console.log("dakogjdogj");
+            loginForm.addEventListener("submit", e => {
                 var username = document.getElementById("username").value;
                 var password = document.getElementById("password").value;
             
@@ -35,15 +47,63 @@ fetch("data.json")
             })
         }
 
+        // Event Listener for deposit / withdraw
         if(transactionForm != null){
-            transactionForm.addEventListener("submit", (e) => {
+            transactionForm.addEventListener("submit", e => {
                 e.preventDefault();
                 var pin = document.getElementById("pin").value;
-
-                if(pin == json.accountData.pin) {
-                    console.log("PIN correct!");
+                // Deposit
+                if(e.submitter.id == "deposit") {
+                    if(document.getElementById("depositAmount").value != ""){
+                        if(document.getElementById("depositAmount").value > 999999999) { // if Amount is too high
+                            document.getElementById("depositAmount").value = "";
+                            document.getElementById("depositAmount").placeholder = "Deposit a lower Amount!"
+                            return;
+                        }
+                        if(pin == json.accountData.pin) {
+                            json.balance = Number(document.getElementById("depositAmount").value) + Number(document.getElementById("currentBalance").innerHTML); // add Amount to Current Balance
+                            
+                            var jsonObj = json.transHistory;
+                            jsonObj.push({"Deposited": document.getElementById("depositAmount").value})
+                            document.getElementById("transactions").innerHTML = ""; // clear Transaction History
+                            
+                            document.getElementById("currentBalance").innerHTML = json.balance; // update Current Balance
+                            for (let i = 1; i < jsonObj.length; i++) {
+                                document.getElementById("transactions").innerHTML += i + ") " + Object.keys(jsonObj[i]) + ": " +
+                                Object.values(jsonObj[i]) + "<br>"; // add all values to Transaction History
+                            }
+                            document.getElementById("depositAmount").value = ""; // clear Money Amount Field
+                        }
+                    }
                 }
-                
+                // Withdraw
+                else if(document.getElementById("withdrawAmount").value != "") {
+                        if(document.getElementById("withdrawAmount").value > 999999999) { // if Amount is too high
+                            document.getElementById("withdrawAmount").value = "";
+                            document.getElementById("withdrawAmount").placeholder = "Withdraw a lower Amount!"
+                            return;
+                        }
+                    if(pin == json.accountData.pin) {
+                        if(Number(document.getElementById("currentBalance").innerHTML) - Number(document.getElementById("withdrawAmount").value) >= 0){ // checks Current Balance - Withdrawel Amount
+                            json.balance = Number(document.getElementById("currentBalance").innerHTML) - Number(document.getElementById("withdrawAmount").value); // subtract Amount of Current Balance
+
+                            var jsonObj = json.transHistory;
+                            jsonObj.push({"Withdrawed": document.getElementById("withdrawAmount").value})
+                            document.getElementById("transactions").innerHTML = ""; // clear Transaction History
+    
+                            document.getElementById("currentBalance").innerHTML = json.balance; // update Current Balance
+                            for (let i = 1; i < jsonObj.length; i++) {
+                                document.getElementById("transactions").innerHTML += Object.keys(jsonObj[i]) + ": " +
+                                Object.values(jsonObj[i]) + "<br>"; // add all values to Transaction History
+                            }
+                            document.getElementById("withdrawAmount").value = ""; // clear Money Amount Field
+                        }
+                        else{ // Current Balance - Withdrawel < 0
+                            document.getElementById("withdrawAmount").value = "";
+                            document.getElementById("withdrawAmount").placeholder = "Not enought Money!";
+                        }
+                    }
+                }
             })
         }
 })
