1f06d3745a38af72499ae7b36dbc213f0d32b5ea
Alexander <alexander.flach2004@web.de>
2024-06-24 10:30:03 +0200

    current balance calculation, saving balance

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ app.get("/register", (req, res) => {
 })
 app.get("/account", (req, res) => {
     data = []
-    res.render("account", {data: data})
+    balanceValue = 0;
+    res.render("account", {data: data, balanceValue: balanceValue})
 })
 
 app.post("/login", async (req, res) => {
@@ -60,10 +61,16 @@ app.post("/register", async (req, res) => {
     const {username, password, pin} = req.body
     const user = await User.findOne({ username: username})
     if(!user) {
-        const user = new User({username, password, pin, balance: 0, history: []})
-        await user.save()
-        console.log("new User created")
-        res.redirect("/login")
+        if(!username || !password || !pin){
+            console.log("Username, Password or PIN missing!")
+            res.redirect("/register")
+        }
+        else{
+            const user = new User({username, password, pin, balance: 0, history: []})
+            await user.save()
+            console.log("new User created")
+            res.redirect("/login")
+        }
     }
     else {
         console.log("User already exists")
@@ -71,7 +78,6 @@ app.post("/register", async (req, res) => {
     }
 })
 
-var historyInit = false
 
 app.post("/account", async (req, res) => {
     const {username, pin, deposit, withdraw} = req.body
@@ -81,6 +87,7 @@ app.post("/account", async (req, res) => {
     
     if(user && user.pin == pin) {
 
+        balanceValue = user.balance
         if(user.history) {
             for(let i = 0; i < 99; i++) {
                 if(user.history[i]) {
@@ -93,11 +100,15 @@ app.post("/account", async (req, res) => {
         if(withdraw != 0) {
             user.history.push({transaction: {type:"withdraw", value: withdraw}})
             data.unshift("withdraw: " + withdraw)
+            balanceValue -= withdraw
+            user.balance = balanceValue
             await user.save()
         }
         if(deposit != 0) {
             user.history.push({transaction: {type:"deposit", value: deposit}})
             data.unshift("deposit: " + deposit)
+            balanceValue += parseInt(deposit)
+            user.balance = balanceValue
             await user.save()
         }
         res.render("account", {data: data})
